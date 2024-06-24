d0b2b4fcabeca1f125bdd0b9f223b2e94711de86
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-24 23:25:59 +0100

    add bio image

--- src/pages/bioanalytical/Bioanalytical.jsx
+++ src/pages/bioanalytical/Bioanalytical.jsx
@@ -4,7 +4,27 @@ import "./bioanalytical.css";
 const Bioanalytical = () => {
   return (
     <>
-      Bioanalytical
+      <div
+        className="single-slider"
+        style={{ backgroundImage: 'url("img/bio.jpg")' }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7">
+              <div className="text">
+                <p>
+                  As the world evolves at an unprecedented pace, the forefront
+                  of innovation lies within the realm of biotechnology. Welcome
+                  to a journey where science meets possibility, where
+                  breakthroughs redefine our understanding of life itself. Join
+                  us as we navigate the endless frontiers of global innovation
+                  in biotechnology, shaping the future one discovery at a time.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
