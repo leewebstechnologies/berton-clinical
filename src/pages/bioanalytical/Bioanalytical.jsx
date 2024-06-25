662a7681699cc3e450adb9c5ff53c10a5fa19c1d
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-25 15:50:00 +0100

    add bio contents

--- src/pages/bioanalytical/Bioanalytical.jsx
+++ src/pages/bioanalytical/Bioanalytical.jsx
@@ -9,23 +9,45 @@ const Bioanalytical = () => {
         <img className="bioimage" src={bio} alt="bio" />
         <h1>Bioanalytical Lab</h1>
         <h3>Empowering Discovery: Global Bioanalytical Innovation Unleashed</h3>
-        <p className="first">
-          Our bioanalytical expertise reaches across continents, providing
-          leading drug development companies with the actionable data and
-          insights needed to accelerate therapies from research to reality.
-          <br />
-          Our commitment to excellence is reflected in every aspect of our
-          operations, from our state-of-the-art facilities and meticulously
-          calibrated technology to our strategic satellite laboratories
-          strategically positioned in key regions worldwide. <br /> At the heart
-          of our operation is our cutting-edge track and trace technology, which
-          enables real-time monitoring and intervention capabilities to ensure
-          the seamless transportation of samples from even the most remote
-          locations to our labs without delay. This innovative approach not only
-          overcomes geographical barriers but also ensures the integrity and
-          reliability of the data generated throughout the bioanalytical
-          process. <br />
-        </p>
+        <div className="first">
+          <p>
+            Our bioanalytical expertise reaches across continents, providing
+            leading drug development companies with the actionable data and
+            insights needed to accelerate therapies from research to reality.
+            <br />
+            Our commitment to excellence is reflected in every aspect of our
+            operations, from our state-of-the-art facilities and meticulously
+            calibrated technology to our strategic satellite laboratories
+            strategically positioned in key regions worldwide. <br /> At the
+            heart of our operation is our cutting-edge track and trace
+            technology, which enables real-time monitoring and intervention
+            capabilities to ensure the seamless transportation of samples from
+            even the most remote locations to our labs without delay. This
+            innovative approach not only overcomes geographical barriers but
+            also ensures the integrity and reliability of the data generated
+            throughout the bioanalytical process.
+          </p>
+          <h5>Advanced Technologies and Methodologies</h5>
+          <p>
+            <b>Cutting-Edge Instrumentation:</b> Our labs are equipped with the
+            latest instrumentation, including high-performance liquid
+            chromatography (HPLC), mass spectrometry (MS), and advanced
+            immunoassay platforms, allowing for precise and sensitive detection
+            of analytes.
+          </p>
+          <p>
+            <b>Multiplexed Assays:</b> We specialize in multiplexed assays,
+            enabling simultaneous measurement of multiple analytes in a single
+            sample, thus maximizing efficiency and conserving precious sample
+            volume.
+          </p>
+          <p>
+            <b>High-Throughput Screening:</b> Leveraging automation and
+            robotics, we offer high-throughput screening services, accelerating
+            the analysis of large sample sets while maintaining accuracy and
+            reproducibility.
+          </p>
+        </div>
       </div>
       <Footer />
     </>
