3c729e0607b326bf592837dccd4708a9c558b915
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-26 18:23:10 +0100

    add image to ophthalmology page

--- src/pages/ophthalmology/Ophthalmology.jsx
+++ src/pages/ophthalmology/Ophthalmology.jsx
@@ -7,6 +7,21 @@ const Ophthalmology = () => {
     <>
       <div className="ophthalmology">
         <img src={ophthalmology} alt="ophthalmology" />
+        <div className="text">
+          <br />
+          <h1>Ophthalmology</h1>
+          <h3>Advancing Cutting-Edge Ophthalmology Treatments</h3>
+        </div>
+        <div className="paragraph">
+          <p>
+            At Berton Clinical, our comprehensive expertise in ophthalmology
+            spans the entire spectrum of clinical research, from Phase I through
+            Phase IV trials, encompassing a wide range of eye diseases and
+            conditions. Our tailored approach to ophthalmic research allows us
+            to address the unique challenges and complexities associated with
+            each study phase and indication.
+          </p>
+        </div>
       </div>
       <Footer />
     </>
