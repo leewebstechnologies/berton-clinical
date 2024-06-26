fe3924ad1ce5e7485e71c8dad38f0c9dd5644aa8
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-26 18:15:27 +0100

    add ophthalmology

--- src/pages/ophthalmology/Ophthalmology.jsx
+++ src/pages/ophthalmology/Ophthalmology.jsx
@@ -1,10 +1,13 @@
 import Footer from "../../components/footer/Footer";
 import "./ophthalmology.css";
+import ophthalmology from "../../images/ophthalmology.jpg";
 
 const Ophthalmology = () => {
   return (
     <>
-      Ophthalmology
+      <div className="ophthalmology">
+        <img src={ophthalmology} alt="ophthalmology" />
+      </div>
       <Footer />
     </>
   );
