902046a4b38eeb54c2eabef14b66b26054b4f953
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-26 09:40:16 +0100

    add clinical research trainings page

--- src/pages/clinical/Clinical.jsx
+++ src/pages/clinical/Clinical.jsx
@@ -1,10 +1,18 @@
 import Footer from "../../components/footer/Footer";
 import "./clinical.css";
+import training from "../../images/training.jpg";
 
 const Clinical = () => {
   return (
     <>
-      Clinical Research
+      <div className="clinical">
+        <img className="training" src={training} alt="training" />
+        <div className="texts">
+          <br />
+          <h1>Clinical Research Professional Trainings</h1>
+          <h3>Launch Your Career in Clinical Research</h3>
+        </div>
+      </div>
       <Footer />
     </>
   );
