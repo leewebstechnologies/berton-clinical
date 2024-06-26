2bfa5af998ba7f7d03b24684645afa4f0e8e8706
leewebstechnologies <aliuajibade317@gmail.com>
2024-06-26 16:02:15 +0100

    add rescue

--- src/pages/clinical/Clinical.jsx
+++ src/pages/clinical/Clinical.jsx
@@ -17,40 +17,108 @@ const Clinical = () => {
             Welcome to Berton Academy, where we specialize in offering
             comprehensive training programs meticulously crafted to equip you
             with the skills and knowledge necessary for a successful and
-            fulfilling career in clinical research. <br /> Our programs are
-            meticulously designed to cover all aspects of the clinical research
-            field, including regulatory compliance, protocol design, data
-            management, and ethical considerations. Led by industry experts with
-            years of practical experience, our courses provide hands-on training
-            and real-world insights that prepare you to excel in this dynamic
-            and rapidly evolving industry. <br /> One of the key advantages of
-            enrolling at Berton Academy is our extensive network of industry
-            connections. We serve as your gateway to the vibrant ecosystem of
-            clinical research professionals, providing unparalleled
-            opportunities for networking, mentorship, and career advancement.
-            Through our connections with leading pharmaceutical companies,
-            research organizations, and regulatory agencies, we ensure that our
-            students have access to the latest trends, best practices, and job
-            opportunities in the field.
-            <br /> At Berton Academy, we understand the value of collaboration
-            and growth within the clinical research community. Our programs not
-            only focus on individual skill development but also foster a
+            fulfilling career in clinical research.
+          </p>
+          <p>
+            Our programs are meticulously designed to cover all aspects of the
+            clinical research field, including regulatory compliance, protocol
+            design, data management, and ethical considerations. Led by industry
+            experts with years of practical experience, our courses provide
+            hands-on training and real-world insights that prepare you to excel
+            in this dynamic and rapidly evolving industry.
+          </p>
+          <p>
+            One of the key advantages of enrolling at Berton Academy is our
+            extensive network of industry connections. We serve as your gateway
+            to the vibrant ecosystem of clinical research professionals,
+            providing unparalleled opportunities for networking, mentorship, and
+            career advancement. Through our connections with leading
+            pharmaceutical companies, research organizations, and regulatory
+            agencies, we ensure that our students have access to the latest
+            trends, best practices, and job opportunities in the field.
+          </p>
+          <p>
+            At Berton Academy, we understand the value of collaboration and
+            growth within the clinical research community. Our programs not only
+            focus on individual skill development but also foster a
             collaborative learning environment where students can exchange
             ideas, share experiences, and build professional relationships that
             last a lifetime.
-            <br /> Whether you're looking to launch your career in clinical
-            research or advance to the next level, Berton Academy is here to
-            support your journey every step of the way. Join us and discover the
-            countless opportunities awaiting you in the exciting world of
-            clinical research.
+          </p>
+          <p>
+            Whether you're looking to launch your career in clinical research or
+            advance to the next level, Berton Academy is here to support your
+            journey every step of the way. Join us and discover the countless
+            opportunities awaiting you in the exciting world of clinical
+            research.
           </p>
           <br />
           <h4>Discover the Possibilities If You're</h4>
-          <ul style={{ listStyleType: "disc;" }}>
+          <ul className="point">
             <li>Registered Nurse</li>
-            <li>College Graduat</li>
+            <li>College Graduate</li>
             <li>Non-Medical Professional</li>
           </ul>
+          <p>
+            We are dedicated to providing targeted training curriculums and
+            comprehensive career support services tailored to your unique
+            background and experience level in the field of clinical research.
+          </p>
+          <p>
+            Our programs are meticulously designed to equip you with the core
+            competencies and skills necessary to thrive in various roles within
+            the clinical research industry. Whether you're looking to transition
+            into a Clinical Research Associate (CRA) role or seeking to advance
+            your career as a Clinical Investigator, we offer specialized
+            training programs to meet your specific needs.
+          </p>
+          <br />
+          <h4>
+            Here's A Glimpse Into Some Of The Key Training Programs We Offer
+          </h4>
+          <p>
+            <b>Clinical Research Associate (CRA):</b> Our training program
+            focuses on developing proficiency in overseeing subject safety,
+            conducting site visits, and monitoring clinical trials. By
+            completing this program, you'll equip yourself with the essential
+            skills needed to excel as a highly professional CRA, setting the
+            stage for advancement into Lead CRA or CRA Manager roles.
+          </p>
+          <p>
+            <b>Clinical Investigator:</b> For those interested in running
+            clinical trials from an investigator perspective, we offer
+            specialized training covering subjects such as subject recruitment,
+            study site management, adverse event reporting, and protocol
+            adherence.
+          </p>
+          <p>
+            <b>Building a Clinical Research Site:</b> Designed for healthcare
+            professionals looking to establish their own study site, this
+            curriculum covers essential topics including feasibility analysis,
+            staffing needs, site set-up, contracting, budgeting, and day-to-day
+            operations.
+          </p>
+          <p>
+            <b>Remediating FDA 483 Observations:</b> Our program provides
+            insights and best practices for implementing quality improvements
+            and corrective actions to address and prevent FDA 483 observations
+            and warning letters, ensuring compliance and operational excellence.
+          </p>
+          <p>
+            In addition to these programs, we will be offering training for
+            roles in data management, pharmacovigilance, medical writing,
+            regulatory affairs, and more, ensuring a comprehensive suite of
+            offerings to support your career advancement goals in clinical
+            research.
+          </p>
+          <br />
+          <p>
+            At Berton Academy, our hands-on, role-focused training approach
+            ensures that you gain the qualifications and practical experience
+            necessary to excel in the competitive field of clinical research.
+            Explore our courses and curriculum today to take the next step
+            towards advancing your clinical research career.
+          </p><br />
         </div>
       </div>
       <Footer />
--- src/pages/rescue/Rescue.jsx
+++ src/pages/rescue/Rescue.jsx
@@ -1,10 +1,77 @@
 import Footer from "../../components/footer/Footer";
 import "./rescue.css";
+import rescue from "../../images/rescue.jpg";
 
 const Rescue = () => {
   return (
     <>
-      Rescue
+      <div className="rescue">
+        <img src={rescue} alt="rescue" />
+        <div className="texts">
+          <br />
+          <h1>Rescue Research - Save Studies</h1>
+          <h3>Get Stalled Clinical Trials Back on Track</h3>
+        </div>
+        <div className="paragraph">
+          <p>
+            Is your clinical trial facing unexpected delays? Are milestones
+            slipping through the cracks with your current research partner? Look
+            no further than Berton Clinical for expert rescue research services
+            designed to swiftly get your studies back on course.
+          </p>
+          <p>
+            Every day of delay in clinical trials translates to lost time and
+            resources. That's why our dedicated team intervenes promptly to
+            revitalize your development program and ensure efficient execution.
+          </p>
+          <h5>
+            As a seasoned Contract Research Organization (CRO), we offer
+            tailored support based on your study's current status
+          </h5>
+          <p>
+            <b>Expedited Startup Assistance:</b> For trials in the startup
+            phase, we streamline site identification, contracting, and
+            activation processes to expedite the enrollment of the first
+            subject.
+          </p>
+          <p>
+            <b>Enhanced Recruitment Strategies:</b> If enrollment is lagging, we
+            leverage our extensive site network and implement targeted promotion
+            to boost recruitment efforts.
+          </p>
+          <p>
+            <b>Data Resolution Solutions:</b> Addressing data issues head-on,
+            our experts perform data migration or cleaning to seamlessly realign
+            data collection efforts.
+          </p>
+          <p>
+            <b>Rescue Monitoring Initiatives:</b> For trials in need of rescue
+            monitoring, our experienced Clinical Research Associates (CRAs)
+            conduct swift site visits and address protocol deviations promptly.
+          </p>
+          <p>
+            <b>Focused Study Completion Support:</b> As your study nears
+            completion, we intensify our focus on resolving outstanding queries,
+            managing adverse events, and ensuring meticulous documentation for
+            faster database lock.
+          </p>
+          <br />
+          <p>
+            Regardless of the stage at which your study stalled, Berton Clinical
+            possesses the experience and resources necessary to stabilize
+            operations immediately. We then work diligently to optimize
+            processes, mitigating the risk of future disruptions.
+          </p>
+          <p>
+            Don't let poor trial performance hinder your critical momentum. Our
+            proactive study rescue services are designed to tackle delays and
+            challenges head-on, ensuring that your development program continues
+            to advance seamlessly. Let our expert team get your trial back on
+            track today.
+          </p>
+          <br />
+        </div>
+      </div>
       <Footer />
     </>
   );
