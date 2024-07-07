284589cd7b1d48db043bdfa2292ff12eb3ff6781
leewebstechnologies <aliuajibade317@gmail.com>
2024-07-07 10:03:02 +0100

    add h3

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -6,14 +6,14 @@ const Landing = () => {
     <>
       <div className="landing">
         <video className="molecules" src={molecules} autoPlay loop muted />
-        <h4 className="heading">
+        <h3 className="heading">
           As the world evolves at an unprecedented pace, the forefront of
           innovation lies within the realm of biotechnology. Welcome to a
           journey where science meets possibility, where breakthroughs redefine
           our understanding of life itself. Join us as we navigate the endless
           frontiers of global innovation in biotechnology, shaping the future
           one discovery at a time.
-        </h4>
+        </h3>
       </div>
     </>
   );
